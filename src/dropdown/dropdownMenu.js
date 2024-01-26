ae6b3c59369fc1051ae450804fbcd3afd0c3d5d5
aishyuu <ivanmatias1331@gmail.com>
2024-01-26 01:36:03 -0800

    Added basic structure to dropdown menu given the data

---
+++ src/dropdown/dropdownMenu.js
@@ -0,0 +1,26 @@
+export default function dropdownMenu(dropdownData) {
+    // Entire minimal-dropdowns div
+    const menuDiv = document.createElement("div");
+    menuDiv.classList.add("minimal-dropdowns-encapsulating-div");
+
+    // minimal-dropdowns button
+    const menuButton = document.createElement("button");
+    menuButton.classList.add("minimal-dropdowns-button");
+    menuButton.innerText = dropdownData.title;
+    menuDiv.appendChild(menuButton);
+
+    // dropdown content div
+    const dropdownContentDiv = document.createElement("div");
+    dropdownContentDiv.classList.add("minimal-dropdowns-content-div")
+
+    /* eslint-disable no-plusplus */
+    for (let index = 0; index < dropdownData.options.length; index++) {
+        const dropdownSlot = document.createElement("a");
+        dropdownSlot.href = dropdownData.references[index];
+        dropdownSlot.innerText = dropdownData.options[index];
+        dropdownSlot.classList.add("minimal-dropdowns-content-slot");
+        dropdownContentDiv.appendChild(dropdownSlot);
+    }
+    menuDiv.appendChild(dropdownContentDiv)
+    return menuDiv
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import "./style.css";
 import "./reset.css";
+import dropdownMenu from "./dropdown/dropdownMenu";
 
 function component() {
-  
+  const body = document.querySelector("body")
+  const data = {
+    title: "Test Menu",
+    options: [
+      "link 1",
+      "link 2",
+      "link 3"
+    ],
+    references: [
+      "https://www.google.com",
+      "https://www.github.com",
+      "https://www.theodinproject.com"
+    ]
+  }
+  const help = dropdownMenu(data)
+  body.appendChild(help)
 }
 
 component();
